bc774b0fc1b498cb6ff9c018ca4b94c14c629243
Chung0754 <Zhongmohan@gmail.com>
2021-12-14 18:08:40 -0600

    Initial commit

---
+++ HELP.md
@@ -0,0 +1,34 @@
+# Getting Started
+
+### Reference Documentation
+
+For further reference, please consider the following sections:
+
+* [Official Gradle documentation](https://docs.gradle.org)
+* [Spring Boot Gradle Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/2.6.1/gradle-plugin/reference/html/)
+* [Create an OCI image](https://docs.spring.io/spring-boot/docs/2.6.1/gradle-plugin/reference/html/#build-image)
+* [Coroutines section of the Spring Framework Documentation](https://docs.spring.io/spring/docs/5.3.13/spring-framework-reference/languages.html#coroutines)
+* [Spring Data JDBC](https://docs.spring.io/spring-data/jdbc/docs/current/reference/html/)
+* [Spring Web](https://docs.spring.io/spring-boot/docs/2.6.1/reference/htmlsingle/#boot-features-developing-web-applications)
+* [Spring Web Services](https://docs.spring.io/spring-boot/docs/2.6.1/reference/htmlsingle/#boot-features-webservices)
+* [Rest Repositories](https://docs.spring.io/spring-boot/docs/2.6.1/reference/htmlsingle/#howto-use-exposing-spring-data-repositories-rest-endpoint)
+
+### Guides
+
+The following guides illustrate how to use some features concretely:
+
+* [Using Spring Data JDBC](https://github.com/spring-projects/spring-data-examples/tree/master/jdbc/basics)
+* [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
+* [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
+* [Building REST services with Spring](https://spring.io/guides/tutorials/bookmarks/)
+* [Producing a SOAP web service](https://spring.io/guides/gs/producing-web-service/)
+* [Accessing JPA Data with REST](https://spring.io/guides/gs/accessing-data-rest/)
+* [Accessing Neo4j Data with REST](https://spring.io/guides/gs/accessing-neo4j-data-rest/)
+* [Accessing MongoDB Data with REST](https://spring.io/guides/gs/accessing-mongodb-data-rest/)
+
+### Additional Links
+
+These additional references should also help you:
+
+* [Gradle Build Scans – insights for your project's build](https://scans.gradle.com#gradle)
+
---
+++ src/main/kotlin/com/example/cs554serverheroku/Cs554serverherokuApplication.kt
@@ -0,0 +1,162 @@
+package com.example.cs554serverheroku
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.repository.query.ReturnedType
+import org.springframework.web.bind.annotation.*
+import kotlin.random.Random
+
+@SpringBootApplication
+class Cs554serverherokuApplication
+
+@Serializable
+data class Component(val id: String, val variant: String, val expiry: String, val itemDataModel: ItemDataModel)
+@Serializable
+data class Layout(val pageID: String, val expiry: String, val components: List<Component>)
+@Serializable
+data class ItemDataModel(val id: String, var text: String, val color:String, val itemName: String, val imageURL:String,
+                         val iconKey: String, val action: String, val payload: List<String>)
+@Serializable
+data class DataModelUpdateRequest(val pageID: String, val id: String)
+
+
+val layout_dict: MutableMap<String, Layout> = mutableMapOf()
+val layouts_json: MutableList<String> = mutableListOf()
+var page01 = Layout("1", "6000", listOf(
+    Component("3265", "label", "6000",
+        ItemDataModel("3244","this is a label","red","","","","",
+            listOf("payload1","payload2"))
+    ),
+    Component("5165", "label", "6000",
+        ItemDataModel("4243","this is another label","green","","","","",
+            listOf())
+    ),
+    Component("4765", "button", "6000",
+        ItemDataModel("5249","tap me","red","","","","action1",
+            listOf())
+    ),
+    Component("4327", "button", "6000",
+        ItemDataModel("3876","tap me","blue","","","","action2",
+            listOf())
+    ),
+    Component("1239", "list", "6000",
+        ItemDataModel("7654","","red","","","","",
+            listOf("item1","item2"))
+    ),
+    Component("6645", "icon", "6000",
+        ItemDataModel("9344","sun rise","","","","sunrise","",
+            listOf())
+    )
+))
+
+var page01_new = Layout("1", "6000", listOf(
+    Component("3265", "label", "6000",
+        ItemDataModel("2","label updated","red","","","","",
+            listOf("payload1","payload2"))
+    ),
+    Component("5165", "label", "6000",
+        ItemDataModel("4243","this is another label","green","","","","",
+            listOf())
+    ),
+    Component("4765", "button", "6000",
+        ItemDataModel("5249","tap me","red","","","","action1",
+            listOf())
+    ),
+    Component("4327", "button", "6000",
+        ItemDataModel("3876","tap me","blue","","","","action2",
+            listOf())
+    ),
+    Component("1239", "list", "6000",
+        ItemDataModel("7654","","red","","","","",
+            listOf("item1","item2"))
+    ),
+    Component("6645", "icon", "6000",
+        ItemDataModel("9344","sun rise","","","","sunrise","",
+            listOf())
+    )
+))
+
+
+
+fun main(args: Array<String>) {
+    layout_dict["1"] = page01
+    runApplication<Cs554serverherokuApplication>(*args)
+}
+
+@RestController
+class LayoutTest01Resource {
+    @GetMapping(value = ["/LayoutTest01"])
+    fun sampleLayout1(): Layout? {
+        return layout_dict["1"]
+    }
+}
+
+@RestController
+class LayoutTest03Resource {
+    @PostMapping(value = ["/LayoutTest03"])
+    fun sampleLayout3(): Layout {
+        return page01_new
+    }
+}
+
+
+@RestController
+class EchoPayloadResource {
+    @PostMapping(value = ["/EchoPayload"])
+    fun echo(@RequestBody payload: String): String{
+        return payload
+    }
+}
+
+@RestController
+class LayoutUpload {
+    @PostMapping(value = ["/LayoutUpload"])
+    fun uploadLayout(@RequestBody payload: String): String{
+        val layout_toAdd = Json.decodeFromString<Layout>(payload)
+        if(layout_dict.containsKey(layout_toAdd.pageID)){
+            return "invalid pageid, follow id are occupied"+ layout_dict.keys
+        }
+        layout_dict[layout_toAdd.pageID] = layout_toAdd
+
+
+
+    return "pageID: "+layout_toAdd.pageID
+    }
+}
+
+@RestController
+class LayoutResourceGetter {
+    @GetMapping(value = ["/LayoutResource/{requested_resource}"])
+    fun getLayout(@PathVariable(value="requested_resource") k: String): Layout? {
+        if (layout_dict.containsKey(k))
+            return layout_dict[k]
+        else
+            return Layout("0000","6000", listOf())
+    }
+}
+
+@RestController
+class ItemModelUpdate {
+    @PostMapping(value = ["/ItemModelUpdate"])
+    fun dataModelUpdate(@RequestBody payload: String): Layout{
+        val update_request = Json.decodeFromString<DataModelUpdateRequest>(payload)
+        if (layout_dict.containsKey(update_request.pageID)){
+            val toUpdate = layout_dict[update_request.pageID]
+            for (c in toUpdate!!.components){
+                if (c.itemDataModel.id == update_request.id){
+                    c.itemDataModel.text += " updated"
+                    break
+                }
+            }
+            layout_dict[update_request.pageID] = toUpdate
+            return toUpdate
+        }else{
+            return page01_new
+        }
+
+
+    }
+}
